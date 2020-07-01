fbd389013f811e184d3ee4c8cc6d39d0368cc235
Lucanatorr <45624810+Lucanatorr@users.noreply.github.com>
2020-07-01 14:42:45 -0400

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,16 @@
+Locales['en'] = {
+    ['blip_name'] = 'food Stand',
+    ['help_text'] = 'press ~INPUT_CONTEXT~ to order food',
+    ['shop_item'] = '$%s',
+    ['shop_title'] = 'food Stand',
+    ['bought'] = 'You have purchased ~b~%sx~s~ ~y~%s~s~ for ~g~$%s~s~',
+    ['not_enough_space'] = 'not enough free space in your inventory',
+    ['not_enough_cash'] = 'you need ~g~$%s~s~ more to purchase',
+    ['shop_confirm'] = 'buy %sx %s for $%s?',
+    ['no'] = 'no',
+    ['yes'] = 'yes',
+    ['used_taco'] = 'you have eaten a taco',
+    ['used_pretzel'] = 'you have eaten a pretzel',
+    ['used_hotdog'] = 'you have eaten a hotdog',
+    ['come_back_later'] = 'the stand is ~r~closed~s~, come back later.'
+}
